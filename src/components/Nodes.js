a4fbc14a796a71f79588b6d08bb6b2bc4533547a
Jan <mail@ja.nz>
2016-08-10 00:01:20 +0200

    formatting

--- src/components/Nodes.js
+++ src/components/Nodes.js
@@ -3,11 +3,11 @@ import React from 'react';
 export default({nodes}) => (
   <div>{
     nodes.map(({value, timestamp, nodeId}) =>
-    <span key={nodeId}>
-      <p>{nodeId}</p>
-      <p>{value}</p>
-      <p>{timestamp}</p>
-    </span>
+      <span key={nodeId}>
+        <p>{nodeId}</p>
+        <p>{value}</p>
+        <p>{timestamp}</p>
+      </span>
     )
   }</div>
 );
