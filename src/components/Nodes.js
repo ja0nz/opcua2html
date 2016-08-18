4b9ad792db779824791d15b51e4b75b0b2c9ec39
Jan <mail@ja.nz>
2016-08-18 11:31:33 +0200

    added addNode

--- src/components/Nodes.js
+++ src/components/Nodes.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
 import Node from './Node';
+import uuid from 'uuid';
 
 export default class Nodes extends Component {
 
-  render() {
-    const {nodeData} = this.props; 
+  constructor(props) {
+    super(props);
+    this.state = {
+      nodesToMount: [
+        {
+          id: uuid.v4(),
+          component: <Node nodes={this.props.nodeData} />
+        }
+      ]
+    }
+  }
 
+
+  render() {
+    const {nodesToMount} = this.state;
     return (
-    <Node nodes={nodeData} />
+      <section>
+      <button onClick={this.addNode}>+</button>
+      <div>{
+        nodesToMount.map(({id, component}) =>
+          <div key={id}>
+          {component}
+          </div>
+        )}</div>
+      </section>
     );
   }
+
+
+  addNode = () => {
+    this.setState({
+      nodesToMount:
+        [...this.state.nodesToMount,
+          {
+            id: uuid.v4(),
+            component: <Node nodes={this.props.nodeData} />
+          }
+        ]
+    });
+  }
+
+
+
+
 }
